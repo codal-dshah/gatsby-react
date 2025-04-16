4a08d0d9e004fd57b0c9f9fbe2cdda12c4137b69
Dhruvi Shah <dshah2@codal.com>
2025-04-16 15:46:28 +0530

    linter setup

--- gatsby-git-project/src/components/Banner.js
+++ gatsby-git-project/src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import './Banner.css';
 
-const Banner = () => {
+const Banner = ({pageID}) => {
   // Fetch page content using GraphQL
   const data = useStaticQuery(graphql`
     query {
